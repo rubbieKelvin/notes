30f42e494318ccf7b1207e3da5fb45f1b2b12da4
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2022-05-02 00:15:43 -0400

    added json-download feature

---
+++ src/composables/useDownload.js
@@ -0,0 +1,23 @@
+import { Editor } from "@tiptap/vue-3";
+
+export default () => {
+  /**
+   * @param {Object} noteObject
+   */
+  const getJSONDownload = (noteObject) => {
+    const blob = new Blob([JSON.stringify(noteObject, null, 2)], {
+      type: "text/json",
+    });
+    const download = `${noteObject.name}.json`;
+    const href = window.URL.createObjectURL(blob);
+
+    return {
+      download,
+      href,
+      dataset: {
+        downloadurl: ["text/json", download, href].join(":"),
+      },
+    };
+  };
+  return { getJSONDownload };
+};
--- src/views/pad.vue
+++ src/views/pad.vue
@@ -26,30 +26,10 @@
             </FuzzyDate>
           </div>
 
-          <div class="tools">
-            <!-- ... -->
-            <IconButton
-              class="hover:bg-red-500 p-1 hover:text-white"
-              @click="deleteModalOpened = true"
-            >
-              <svg
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class=""
-              >
-                <path
-                  d="M3 5.25H15M14.25 5.25L13.5997 14.3565C13.5728 14.7349 13.4035 15.0891 13.1258 15.3477C12.8482 15.6063 12.4829 15.75 12.1035 15.75H5.8965C5.5171 15.75 5.1518 15.6063 4.87416 15.3477C4.59653 15.0891 4.42719 14.7349 4.40025 14.3565L3.75 5.25H14.25ZM7.5 8.25V12.75V8.25ZM10.5 8.25V12.75V8.25ZM11.25 5.25V3C11.25 2.80109 11.171 2.61032 11.0303 2.46967C10.8897 2.32902 10.6989 2.25 10.5 2.25H7.5C7.30109 2.25 7.11032 2.32902 6.96967 2.46967C6.82902 2.61032 6.75 2.80109 6.75 3V5.25H11.25Z"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </IconButton>
-          </div>
+          <NoteTools
+            @click:delete="modals.delete = true"
+            @click:share="modals.share = true"
+          />
         </div>
 
         <!-- ...pad -->
@@ -100,17 +80,24 @@
       </template>
     </div>
 
-    <Modal v-model="deleteModalOpened" dim closeOnClickOutside closeOnEsc>
+    <!-- modals -->
+    <!-- delete modal -->
+    <Modal v-model="modals.delete" dim closeOnClickOutside closeOnEsc>
       <ConfirmDialog
-        :noAction="() => (deleteModalOpened = false)"
+        :noAction="() => (modals.delete = false)"
         :yesAction="() => delete_note()"
         :yesClass="['bg-red-400 hover:bg-red-500']"
         :noClass="['bg-gray-200 hover:bg-gray-300 text-black']"
         message="You cannot undo this action, are you sure you want to delete this note?"
-        :title="`Delete ${note.name}`"
+        :title="`Delete ${note ? note.name : 'Note'}`"
         noText="No, Take me back"
         yesText="Yes, Delete this note"
       />
+    </Modal>
+
+    <!-- share modal -->
+    <Modal v-model="modals.share" dim closeOnClickOutside closeOnEsc>
+      <ShareNote :note="note" @close:modal="modals.share = false" />
     </Modal>
   </div>
 </template>
@@ -127,7 +114,9 @@ import LeftChevronSvg from "../assets/svgs/leftChevronSvg.vue";
 import EmptyStateSvg from "../assets/svgs/emptyStateSvg.vue";
 import { useRouter } from "vue-router";
 import Modal from "@/components/Modal.vue";
-import ConfirmDialog from "@/components/ConfirmDialog.vue";
+import ConfirmDialog from "@/components/modals/ConfirmDialog.vue";
+import NoteTools from "@/components/menu/NoteTools.vue";
+import ShareNote from "@/components/modals/ShareNote.vue";
 
 export default {
   props: {
@@ -146,12 +135,17 @@ export default {
     FuzzyDate,
     Modal,
     ConfirmDialog,
+    NoteTools,
+    ShareNote,
   },
   setup(props) {
     const { Note, getAuthorFullName, deleteNote } = useNote();
     const note = Note(props.ld);
     const router = useRouter();
-    const deleteModalOpened = ref(false);
+    const modals = ref({
+      delete: false,
+      share: false,
+    });
 
     const author = getAuthorFullName(note.value?.author);
 
@@ -184,7 +178,7 @@ export default {
       author,
       noteHeading,
       delete_note,
-      deleteModalOpened,
+      modals,
     };
   },
 };
