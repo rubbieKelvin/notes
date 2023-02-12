f6a52d7e992bbad745f7b6159d3ec8573605b032
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2023-02-12 18:42:40 +0100

    chore: create default note for new users

--- api/apps.py
+++ api/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "api"
+
+    def ready(self) -> None:
+        from api.signals import users
+
+        return super().ready()
