6275f82bbc038b27c862c81e41ce3ab6d24d4ed0
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2023-02-13 17:11:08 +0100

    chore: updated admin

--- api/admin.py
+++ api/admin.py
@@ -6,16 +6,77 @@ from .models import Note, User, Upload, SharedNote
 # admin.site.register([Note, User, Upload, SharedNote])
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    pass
+    ordering = ["title", "readable_id"]
+
+    search_fields = [
+        "title__icontains",
+        "author__username__icontains",
+    ]
+
+    list_display = [
+        "title",
+        "readable_id",
+        "author",
+        "date_created",
+        "last_updated",
+        "is_starred",
+        "is_deleted",
+        "is_trashed",
+        "is_archived",
+        "is_public",
+    ]
+
+    list_filter = [
+        "date_created",
+        "last_updated",
+        "is_starred",
+        "is_deleted",
+        "is_trashed",
+        "is_archived",
+        "is_public",
+        "author",
+    ]
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    ordering = ["username"]
+
+    search_fields = ["username__icontains"]
+
+    list_display = [
+        "username",
+        "is_active",
+        "date_created",
+    ]
+
+    list_filter = [*list_display]
+
 
 @admin.register(Upload)
 class UploadAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ["note__title__icontains", "author__username__icontains"]
+    list_display = ["id", "note", "author", "date_uploaded"]
+
+    list_filter = ["author", "date_uploaded"]
+
 
 @admin.register(SharedNote)
 class SharedNoteAdmin(admin.ModelAdmin):
-    pass
+    ordering = ["shared_to__username"]
+
+    search_fields = [
+        "shared_to__username__icontains",
+        "note__title__icontains",
+        "note__author__username__icontains",
+    ]
+
+    list_display = [
+        "note",
+        "shared_to",
+        "is_active",
+        "allow_edit",
+        "shared_on",
+    ]
+
+    list_filter = [*list_display]
